d6160fcab78aef80107c136780043d96c5693241
Quan Nguyen <quannm@berkeley.edu>
2025-08-21 09:23:02 -0700

    Fix initial bugs

---
+++ newton.py
@@ -0,0 +1,24 @@
+def my_function(x):
+    return 2 * x**2 - 45 * x + 7
+
+
+def derivative(x, my_function, epsilon=0.00001):
+    return (my_function(x + epsilon) - my_function(x)) / (epsilon)
+
+
+def approximation_t(t):
+    return t - derivative(t, my_function, epsilon) / (
+        derivative(t, derivative(t, my_function, epsilon), epsilon)
+    )
+
+
+def optimize():
+    """ 
+    adding doc string
+    """
+    differecnce = approximation_t(x_0)
+    epsilon = 0.01
+    while difference > threshold:
+        x_0 = x_0 + epsilon
+        difference = approximation(x_0)
+    return approximation(x_0)
